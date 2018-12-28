ea42ad4c681b3042a654a02bd937f471c95addcb
Mac User <macuser@Macs-MacBook-Pro.local>
2018-12-27 20:52:29 -0500

    Initial Commit

---
+++ SchoolhouseSkateboarderTests/SchoolhouseSkateboarderTests.swift
@@ -0,0 +1,34 @@
+//
+//  SchoolhouseSkateboarderTests.swift
+//  SchoolhouseSkateboarderTests
+//
+//  Created by Mac User on 12/27/18.
+//  Copyright © 2018 Mac User. All rights reserved.
+//
+
+import XCTest
+@testable import SchoolhouseSkateboarder
+
+class SchoolhouseSkateboarderTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
